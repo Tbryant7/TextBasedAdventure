4c89ec28d68ba30f57e0040490d2073cd0af623b
JeremyGerm <jeremyrwiening@gmail.com>
2024-03-22 15:22:39 -0400

    Added go down option

--- TextBasedAdventure.java
+++ TextBasedAdventure.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class TextBasedAdventure {
     boolean hasSword = false;
-    boolean hasShovel = false;
-
     Scanner keyboardInput = new Scanner(System.in);
 
     public void execute() {
@@ -14,7 +12,7 @@ public class TextBasedAdventure {
     }
 
     public void start() {
-        System.out.println("You find yourself in a large room. What would you like to do?\n1. Go left \n2. Go right \n3. Go up");
+        System.out.println("You find yourself in a large room. What would you like to do?\n1. Go left \n2. Go right\n3. Go down");
         int input = keyboardInput.nextInt();
         if (input == 1) {
             goLeft();
@@ -22,15 +20,13 @@ public class TextBasedAdventure {
         else if (input == 2) {
             goRight();
         }
-        else { 
-            goUp();
+        else if (input == 3) {
+            goDown();
         }
-
-        }
-    
+    }
 
     public void goLeft() {
-        System.out.println("Oh no! You run into a giant! Fight or flight?\n1. Fight \n2. Flight \n3. Dig Out");
+        System.out.println("Oh no! You run into a giant! Fight or flight?\n1. Fight \n2. Flight");
         int input = keyboardInput.nextInt();
         if (input == 1) {
             fight();
@@ -38,32 +34,27 @@ public class TextBasedAdventure {
         else if (input == 2) {
             start();
         }  
-        else if(input == 3) {
-            dig();
-        }
     }
 
     public void goRight() {
         if (!hasSword) {
             System.out.println("You find a sword on the ground!");
             hasSword = true;
-            System.out.println("You also find a Shovel on the ground!");
-            hasShovel = true;
         }
         else {
             System.out.println("There's nothing here...");
         }
         start();
     }
-    public void goUp(){
-        System.out.println("You have ran into a dark abyss, turn back or continue foward?\n1. Turn back \n2. Continue Foward ");
-        int input = keyboardInput.nextInt();
-        if (input == 1){
-            start();
+
+    public void goDown() {
+        if (hasSword) {
+            System.out.println("The door is locked, you can cut the lock with the sword!");
         }
-        else if (input ==2){
-            cont();
+        else {
+            System.out.println("The door is locked, find a sword to cut the lock...");
         }
+        start();
     }
 
     public void fight() {
@@ -71,21 +62,6 @@ public class TextBasedAdventure {
             System.out.println("You defeat the giant with your sword and run out of the cave!");
         } else {
             System.out.println("You get stomped by the giant and red stuff goes everywhere.");
-        }
-    }
-    public void cont(){
-        if(hasShovel){
-            System.out.println("You can now dig out and safely leave, congrats!");
-        }
-        System.out.println("You have venutured to far and don't have a tool good luck getting out now.");
-    }
-
-    public void dig() {
-        if(!hasShovel) {
-            System.out.println("you get crushed by the giant you did not have the shovel");
-        }
-        else if(hasShovel) {
-            System.out.println("you dig out of the cave");
         }
     }
 
