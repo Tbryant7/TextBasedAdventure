77d42a3b67d3ee81a787566707f641610105befd
JeremyGerm <jeremyrwiening@gmail.com>
2024-03-26 16:09:22 -0400

    added go up option as well as fixed dig function and end of game event

--- TextBasedAdventure.java
+++ TextBasedAdventure.java
@@ -14,7 +14,7 @@ public class TextBasedAdventure {
     }
 
     public void start() {
-        System.out.println("You find yourself in a large room. What would you like to do?\n1. Go left \n2. Go right\n3. Go down");
+        System.out.println("You find yourself in a large room. What would you like to do?\n1. Go left \n2. Go right\n3. Go down\n4. Go up");
         int input = keyboardInput.nextInt();
         if (input == 1) {
             goLeft();
@@ -25,11 +25,14 @@ public class TextBasedAdventure {
         else if (input == 3) {
             goDown();
         }
+        else if (input == 4) {
+            goUp();
+        }
     }
 
     public void goLeft() {
 
-        System.out.println("Oh no! You run into a giant! Fight or flight?\n1. Fight \n2. Flight \n3. Dig Out \n4. Throw potion");
+        System.out.println("Oh no! You run into a giant! Fight or flight?\n(This giant holds the key to escaping the cave!)\n1. Fight \n2. Flight \n3. Dig Out \n4. Throw potion");
         int input = keyboardInput.nextInt();
         if (input == 1) {
             fight();
@@ -71,9 +74,16 @@ public class TextBasedAdventure {
         start();
     }
 
+    public void goUp() {
+        System.out.println("You see some light coming from a crack in the ceiling, if you have a shovel you can escape!");
+        dig();
+        }
+
     public void fight() {
         if (hasSword) {
-            System.out.println("You defeat the giant with your sword and run out of the cave!");
+            System.out.println("You defeat the giant with your sword and run out of the cave!\nYou also collect a shovel as a spoil of battle!");
+            hasShovel = true;
+            start();
         } else {
             System.out.println("You get stomped by the giant and red stuff goes everywhere.");
         }
@@ -84,7 +94,17 @@ public class TextBasedAdventure {
             System.out.println("you get crushed by the giant you did not have the potion.");
         }
         else if(hasPotion) {
-            System.out.println("you throw the potion at the giant and it turns into a mouse. you escape ");
+            System.out.println("you throw the potion at the giant and it turns into a mouse. you escape \nYou also collect a shovel as a spoil of battle!");
+            hasShovel = true;
+            start();
+        }
+    }
+    public void dig() {
+        if (!hasShovel) {
+            System.out.println("You need a shovel to dig! DUH!");
+            start();
+        } else {
+            System.out.println("You manage to escape from the cave!");
         }
     }
 
